362727427ae657db8ae92ccad7294caa6194a8af
Paulo Siroy <pausiroy@gmail.com>
2022-09-30 04:15:17 -0700

    Add reset and submit functions for the form

--- main.js
+++ main.js
@@ -3,16 +3,31 @@
 let myLibrary = [];
 
 // Object Constructor
-function Book(title, author, pages, read) {
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = read;
+function Book(Title, Author, Pages, Read) {
+    this.Title = Title;
+    this.Author = Author;
+    this.Pages = Pages;
+    this.Read = Read;
 }
+
+// function for adding book to array
+
+function addBooktoLibrary(Title, Author, Pages, Read) {
+    let book = new Book(Title, Author, Pages, Read);
+    myLibrary.push(book);
+    displayBooksOnPage();
+}
+
 
 //function to display Library array to cards
 function displayBooksOnPage() {
     const books = document.querySelector(".books");
+
+    //Remove all previously displayed cards before I loop over array
+    const removeDivs = document.querySelectorAll(".card");
+    for(let i = 0; i < removeDivs.length; i++) {
+        removeDivs[i].remove();
+    }
 
     //loop over library array and display cards
     myLibrary.forEach(myLibrary => {
@@ -27,19 +42,44 @@ function displayBooksOnPage() {
     })
 }
 // Start event listener/display form to add a new book to library
-const addBookButton = document.querySelector("add-book-button");
+const addBookButton = document.querySelector(".add-book-button");
 addBookButton.addEventListener("click", displayTheForm);
 
 function displayTheForm() {
     document.getElementById("add-book-form").style.display = "";
 }
 
-// function for adding book to array
+// Start event listener/display form to add a new book to library
+const submitButton = document.querySelector(".submit-button");
+submitButton.addEventListener("click", intakeFormData);
 
-function addBooktoLibrary(title, author, pages, read) {
-    let book = new Book(title, author, pages, read);
-    myLibrary.push(book);
-    displayBooksOnPage();
+// Transform form data into variables for intake
+
+function intakeFormData() {
+    let Title = document.getElementById("Title").value;
+    let Author = document.getElementById("Author").value;
+    let Pages = document.getElementById("Pages").value;
+    let Read = document.getElementById("Read").value;
+
+    // Break out if form is incomplete
+    if((Title == "") || (Author == "") || (Pages == "") || (Read == "")) {
+        return;
+    }
+
+    // Call function to input the book data to array
+    addBooktoLibrary(Title, Author, Pages, Read);
+
+    // Reset form after successful submission
+    document.getElementById("add-book").reset();
+
+}   
+
+// Start event listener for clear form button 
+const clearButton = document.querySelector(".reset-button");
+clearButton.addEventListener("click", clearForm);
+
+function clearForm(){
+    document.getElementById("reset-button")
 }
 
 //addBooktoLibrary("The Hobbit", "J.R.R. Tolkien", "295 Pages", "Not Read yet");
